6dbe2d89c7f8c06dac637ee50c64224ce8fb53af
Mridul Roy <midlry.mr@gmail.com>
2023-07-08 12:51:56 +0530

    Updates

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ port=$1
 kill -9 $(lsof -i :$port | grep $port | awk '{print $2}')
 JENKINS_NODE_COOKIE=dontKillMe PORT=$port nohup npm run start &
 a=$(curl localhost:$1:wq/health-check)
-until [ $a -lt 'OK' ]
+until [ $a -eq 'OK' ]
 do
    sleep 1s
 done
