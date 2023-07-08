370def5747810433160fe6a71936b6eaf821429f
Mridul Roy <midlry.mr@gmail.com>
2023-07-08 12:47:46 +0530

    Updates

--- run.sh
+++ run.sh
@@ -2,3 +2,8 @@
 port=$1
 kill -9 $(lsof -i :$port | grep $port | awk '{print $2}')
 JENKINS_NODE_COOKIE=dontKillMe PORT=$port nohup npm run start &
+a=$(curl localhost:$1:wq/health-check)
+until [ $a -lt 'OK' ]
+do
+   sleep 1s
+done
