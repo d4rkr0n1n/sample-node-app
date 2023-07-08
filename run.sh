1cf309f7c2f2945ccc089e072ad0e434aa02da8b
Mridul Roy <midlry.mr@gmail.com>
2023-07-08 13:04:10 +0530

    updates

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 port=$1
 kill -9 $(lsof -i :$port | grep $port | awk '{print $2}')
 JENKINS_NODE_COOKIE=dontKillMe PORT=$port nohup npm run start &
-a=$(curl localhost:$1:wq/health-check)
+a=$(curl localhost:$1/health-check)
 b='OK'
 until [ "$a" = "$b" ]
 do
